c68a875916d05be00df5c84abb1374d51db0e068
tirimid <q44q44q44q44@gmail.com>
2023-11-03 07:21:53 +0000

    progess on markdown mode

--- src/hl/hl_md.c
+++ src/hl/hl_md.c
@@ -5,6 +5,7 @@
 #include "conf.h"
 #include "draw.h"
 
+#define A_CODEBLOCK (A_YELLOW | A_BGOF(CONF_A_NORM))
 #define A_HEADING (A_MAGENTA | A_BGOF(CONF_A_NORM) | A_BRIGHT)
 #define A_BLOCK (A_WHITE | A_BGOF(CONF_A_NORM) | A_DIM)
 #define A_LINK (A_WHITE | A_BRED)
@@ -19,6 +20,7 @@ static int hl_link(wchar_t const *src, size_t len, size_t *i, size_t *out_lb, si
 static int hl_img(wchar_t const *src, size_t len, size_t *i, size_t *out_lb, size_t *out_ub, uint16_t *out_a);
 static int hl_ulist(wchar_t const *src, size_t len, size_t *i, size_t *out_lb, size_t *out_ub, uint16_t *out_a);
 static int hl_olist(wchar_t const *src, size_t len, size_t *i, size_t *out_lb, size_t *out_ub, uint16_t *out_a);
+static size_t firstlnch(wchar_t const *src, size_t len, size_t pos);
 
 int
 hl_md_find(wchar_t const *src, size_t len, size_t off, size_t *out_lb,
@@ -31,7 +33,7 @@ hl_md_find(wchar_t const *src, size_t len, size_t off, size_t *out_lb,
 		} else if (src[i] == L'>') {
 			if (!hl_block(src, len, &i, out_lb, out_ub, out_a))
 				return 0;
-		} else if (i < len - 3 && !wcscmp(&src[i], L"```")) {
+		} else if (i < len - 2 && !wcsncmp(&src[i], L"```", 3)) {
 			if (!hl_codeblock(src, len, &i, out_lb, out_ub, out_a))
 				return 0;
 		} else if (src[i] == L'!') {
@@ -46,6 +48,9 @@ hl_md_find(wchar_t const *src, size_t len, size_t off, size_t *out_lb,
 		} else if (src[i] == L'[') {
 			if (!hl_link(src, len, &i, out_lb, out_ub, out_a))
 				return 0;
+		} else if (src[i] == L'\\') {
+			++i;
+			continue;
 		}
 	}
 	
@@ -54,23 +59,89 @@ hl_md_find(wchar_t const *src, size_t len, size_t off, size_t *out_lb,
 
 static int
 hl_codeblock(wchar_t const *src, size_t len, size_t *i, size_t *out_lb,
-             size_t *out_ub, uint16_t *a)
+             size_t *out_ub, uint16_t *out_a)
 {
+	if (firstlnch(src, len, *i) != *i)
+		return 1;
+	
+	for (size_t j = *i + 3; j < len - 2; ++j) {
+		if (src[j] == L'\\') {
+			++j;
+			continue;
+		} else if (!wcsncmp(&src[j], L"```", 3)) {
+			if (firstlnch(src, len, j) != j)
+				continue;
+			
+			for (size_t k = j + 3; k < len && src[k] != L'\n'; ++k) {
+				if (!iswspace(src[k]))
+					goto skip;
+			}
+			
+			*out_lb = *i;
+			*out_ub = j + 3;
+			*out_a = A_CODEBLOCK;
+			
+			return 0;
+		}
+	skip:;
+	}
+	
 	return 1;
 }
 
 static int
 hl_heading(wchar_t const *src, size_t len, size_t *i, size_t *out_lb,
-           size_t *oub_ub, uint16_t *out_a)
+           size_t *out_ub, uint16_t *out_a)
 {
-	return 1;
+	if (firstlnch(src, len, *i) != *i)
+		return 1;
+
+	size_t j = *i + 1;
+	while (j < len && src[j] == L'#')
+		++j;
+	if (j >= len || !iswspace(src[j]) || src[j] == L'\n')
+		return 1;
+	
+	while (j < len && src[j] != L'\n')
+		++j;
+
+	*out_lb = *i;
+	*out_ub = j;
+	*out_a = A_HEADING;
+	
+	return 0;
 }
 
 static int
 hl_block(wchar_t const *src, size_t len, size_t *i, size_t *out_lb,
          size_t *out_ub, uint16_t *out_a)
 {
-	return 1;
+	if (firstlnch(src, len, *i) != *i)
+		return 1;
+	
+	size_t j = *i + 1;
+	while (j < len && iswspace(src[j]) && src[j] != L'\n')
+		++j;
+	if (j >= len || iswspace(src[j]))
+		return 1;
+	
+	for (;;) {
+		while (j < len && src[j] != L'\n')
+			++j;
+		
+		j += j < len;
+		while (j < len && iswspace(src[j]) && src[j] != L'\n')
+			++j;
+	
+		if (j >= len || src[j] == L'\n')
+			break;
+	}
+	
+	*out_lb = *i;
+	*out_ub = j;
+	*out_a = A_BLOCK;
+	
+	return 0;
 }
 
 static int
@@ -99,4 +170,18 @@ hl_olist(wchar_t const *src, size_t len, size_t *i, size_t *out_lb,
          size_t *out_ub, uint16_t *out_a)
 {
 	return 1;
+}
+
+static size_t
+firstlnch(wchar_t const *src, size_t len, size_t pos)
+{
+	size_t ln = pos;
+	while (ln > 0 && src[ln] != L'\n')
+		--ln;
+	
+	size_t firstch = ln;
+	while (firstch < len && iswspace(src[firstch]))
+		++firstch;
+	
+	return firstch;
 }
